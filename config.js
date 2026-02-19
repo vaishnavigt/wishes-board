925ae56686f13e70a846ee0173d9e72ced6ef9ba
vaishnavigt <vaishnavi.gt@gmail.com>
2026-02-19 13:59:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.WISHES_CONFIG = {
   "eventName": "MohanBabu & Priya <br> Gangadharan & Vaishnavi",
-  "eventDate": "With love and blessings — 2026",
+  "eventDate": "With love and blessings — 22/02/2026",
   "wishFormUrl": "https://forms.office.com/r/yourFormId"
 };
