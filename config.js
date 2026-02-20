d307b342ce2f15bbaa8463fe457d4447be4ed96e
vaishnavigt <vaishnavi.gt@gmail.com>
2026-02-20 18:42:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-window.WISHES_CONFIG = {
-  "eventName": "MohanBabu & Priya <br> Gangadharan & Vaishnavi",
-  "eventDate": "With love and blessings — 2026",
-  "wishFormUrl": "https://forms.office.com/r/7apcbEGixC",
-  "uploadcarePublicKey": "demopublickey"
-};
+window.WISHES_CONFIG = {"eventName": "M", "eventDate": "", "wishFormUrl": "https://forms.office.com/r/7apcbEGixC", "uploadcarePublicKey": "ab707fc88c9eed50f67e"};
